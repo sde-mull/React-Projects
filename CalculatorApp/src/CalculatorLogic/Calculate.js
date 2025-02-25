6faf379afdc2b90c67ff11b6b26c19306ed64113
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2025-02-25 00:18:47 +0000

    First time using mui react for styling the website, understood better the states and useRef. Still need to practice more to structure better the code and make it more reasonable. For now i will let it be like this for proof of my evolution and have something to compare my future projects and see how much better i got at coding react. Still frontend is not my objetve so i am not looking to know about react in every aspect

--- CalculatorApp/src/CalculatorLogic/Calculate.js
+++ CalculatorApp/src/CalculatorLogic/Calculate.js
@@ -2,15 +2,24 @@
 
 function Calculate(operator, numbers){
 
+	let result = null;
+
 	switch(operator){
 		case '+':
-			return ((Number(numbers[0]) + Number(numbers[1])).toString());
+			result = Number(numbers[0]) + Number(numbers[1]);
+			return (result % 1 === 0) ? result.toString() : result.toFixed(2).toString();
 		case "-":
-			return ((Number(numbers[0]) - Number(numbers[1])).toString());
+			result = Number(numbers[0]) - Number(numbers[1]);
+			return (result % 1 === 0) ? result.toString() : result.toFixed(2).toString();
 		case "/":
-			return ((Number(numbers[0]) / Number(numbers[1])).toString());
+			result = Number(numbers[0]) / Number(numbers[1]);
+			return (result % 1 === 0) ? result.toString() : result.toFixed(2).toString();
 		case "*":
-			return ((Number(numbers[0]) * Number(numbers[1])).toString());
+			result = Number(numbers[0]) * Number(numbers[1]);
+			return (result % 1 === 0) ? result.toString() : result.toFixed(2).toString();
+		case "%":
+			result = Number(numbers[0]) % Number(numbers[1]);
+			return (result % 1 === 0) ? result.toString() : result.toFixed(2).toString();
 		default:
 			console.log("Can't do this type of operation: " + operator);
 	}
--- CalculatorApp/src/Components/Calculator.jsx
+++ CalculatorApp/src/Components/Calculator.jsx
@@ -1,24 +1,102 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import { useTheme } from "@mui/material/styles";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Grid2 from "@mui/material/Grid2";
 import Button from "@mui/material/Button";
 import Calculate from '../CalculatorLogic/Calculate'
 
 function Calculator() {
 
-  const initialObj = {
+  const baseStruct = {
 	'numbers': [],
-	'operation': '',
-	'result': 0
+	'operator': null,
+	'result': null,
   };
+
   const theme = useTheme();
   const [display, setDisplay] = useState("0");
-  const [structure, setStructure] = useState(initialObj);
+  const structure = useRef(baseStruct);
+  const [firstInput, setFirstInput] = useState(true);
+
 
   function handleButtonClick(item) {
 
+	if (item === "DEL"){
+		if (!firstInput){
+			setDisplay(display.slice(0,-1));
+		}
+	}
+	else if (item === "="){
+
+		if (display && !firstInput && structure.current.numbers.length < 2){
+			structure.current.numbers.push(display);
+		}
+	
+		if (structure.current.numbers.length === 2 && structure.current.operator){
+			const result = Calculate(structure.current.operator ? structure.current.operator : item, structure.current.numbers);
+			setDisplay(result);
+			structure.current.result = result;
+			structure.current.numbers.pop();
+			structure.current.numbers[0] = result;
+			structure.current.operator = null;
+		}
+
+		setFirstInput(true);
+
+	}
+	else if (item === "C"){
+		structure.current = baseStruct;
+		setDisplay("0");
+	}
+	else if (item === "+" || item === "-" || item === "*" || item === "/" || item === "%"){
+
+		if (display && !firstInput && structure.current.numbers.length < 2){
+			structure.current.numbers.push(display);
+			setDisplay('0');
+		}
+	
+		if (structure.current.numbers.length === 2){
+			const result = Calculate(structure.current.operator ? structure.current.operator : item, structure.current.numbers);
+			setDisplay(result);
+			structure.current.result = result;
+			structure.current.numbers.pop();
+			structure.current.numbers[0] = result;
+		}
+
+		structure.current.operator = item;
+
+		setFirstInput(true);
+		
+	} 
+	else {
+		setFirstInput(false);
+
+		if (firstInput && !structure.current.operator){
+			structure.current.numbers = [];
+		}
+
+		setDisplay((prev) => {{
+
+			if (prev === '0' && item === '0'){
+				return item;
+			}
+
+			if ((prev === '0' && item != '0' && item != '.') || prev === structure.current.result){
+				return(item);
+			}
+
+			if (prev.length === 12){
+				return(prev);
+			}
+
+			if (prev.includes(".") && item === '.'){
+				return (prev);
+			}
+
+			return (prev + item);
+		}})
+	}
   }
   
   return (
@@ -52,31 +130,43 @@ function Calculator() {
         {display}
         </Box>
 		<Grid2 container spacing={1} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-          {["7", "8", "9", "/"].map((item) => (
+		{["DEL", "C", "%", "/"].map((item) => (
             <Grid2 xs={3} key={item}>
-              <Button variant="contained" sx={{ width: "135px", height: "200px", fontSize: "2rem"}} onClick={() => handleButtonClick(item)}>
+              <Button
+                sx={{ width: "135px", height: "160px", fontSize: "2rem"}}
+                variant="contained"
+                color={item === "C" || item === 'DEL' ? "error" : item === "=" ? "success" : "primary"}
+                onClick={() => handleButtonClick(item)}
+              >
                 {item}
               </Button>
             </Grid2>
           ))}
-          {["4", "5", "6", "*"].map((item) => (
+          {["7", "8", "9", "*"].map((item) => (
             <Grid2 xs={3} key={item}>
-              <Button sx={{ width: "135px", height: "200px", fontSize: "2rem"}} variant="contained" onClick={() => handleButtonClick(item)}>
+              <Button variant="contained" sx={{ width: "135px", height: "160px", fontSize: "2rem"}} onClick={() => handleButtonClick(item)}>
+                {item}
+              </Button>
+            </Grid2>
+          ))}
+          {["4", "5", "6", "+"].map((item) => (
+            <Grid2 xs={3} key={item}>
+              <Button sx={{ width: "135px", height: "160px", fontSize: "2rem"}} variant="contained" onClick={() => handleButtonClick(item)}>
                 {item}
               </Button>
             </Grid2>
           ))}
           {["1", "2", "3", "-"].map((item) => (
             <Grid2 xs={3} key={item}>
-              <Button sx={{ width: "135px", height: "200px", fontSize: "2rem"}} variant="contained" onClick={() => handleButtonClick(item)}>
+              <Button sx={{ width: "135px", height: "160px", fontSize: "2rem"}} variant="contained" onClick={() => handleButtonClick(item)}>
                 {item}
               </Button>
             </Grid2>
           ))}
-          {["0", "C", "=", "+"].map((item) => (
-            <Grid2 xs={3} key={item}>
+          {["0", ".", "="].map((item) => (
+            <Grid2 xs={item === "=" ? 6 : 3} key={item}>
               <Button
-                sx={{ width: "135px", height: "200px", fontSize: "2rem"}}
+                sx={{ width: item === '=' ? "270px" : "135px", height: "160px", fontSize: "2rem"}}
                 variant="contained"
                 color={item === "C" ? "error" : item === "=" ? "success" : "primary"}
                 onClick={() => handleButtonClick(item)}
